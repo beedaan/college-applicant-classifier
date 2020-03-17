0fa0697f8a51004cc7fc8f6e4def0f5156d8ecee
Brendan Heussler <brendan@lifejourney.us>
2020-03-17 15:19:37 -0700

    Add validation during construction that gpa cannot be greater than gpaScale

--- src/main/java/com/builtbybrendan/collegeApplicantClassifier/application/Application.java
+++ src/main/java/com/builtbybrendan/collegeApplicantClassifier/application/Application.java
@@ -2,10 +2,11 @@ package com.builtbybrendan.collegeApplicantClassifier.application;
 
 import com.builtbybrendan.collegeApplicantClassifier.applicationStatus.ApplicationStatus;
 import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
-@Builder
+@Getter
+@Setter
 public class Application {
 
     public String firstName;
@@ -13,10 +14,28 @@ public class Application {
     public State state;
     public int age;
     public double gpa;
-    // TODO validate gpaScale is >= gpa
     public double gpaScale;
     public int satScore;
     public int actScore;
     public int felonies;
     public ApplicationStatus applicationStatus;
+
+    @Builder
+    private static Application create(String firstName, String lastName, State state, int age, double gpa,
+                                      double gpaScale, int satScore, int actScore, int felonies) {
+        Application application = new Application();
+        application.setFirstName(firstName);
+        application.setLastName(lastName);
+        application.setState(state);
+        application.setAge(age);
+        application.setSatScore(satScore);
+        application.setActScore(actScore);
+        application.setFelonies(felonies);
+
+        if (gpaScale < gpa) {
+            throw new IllegalArgumentException("GPA cannot be greater than GPA Scale");
+        }
+
+        return application;
+    }
 }

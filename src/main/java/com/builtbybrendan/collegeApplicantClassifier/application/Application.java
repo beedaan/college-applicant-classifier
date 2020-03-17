4ee5848cdd45e2b510f17c31d58b3eef3484f446
Brendan Heussler <brendan@lifejourney.us>
2020-03-17 16:29:31 -0700

    Modify Application so tests pass

--- src/main/java/com/builtbybrendan/collegeApplicantClassifier/application/Application.java
+++ src/main/java/com/builtbybrendan/collegeApplicantClassifier/application/Application.java
@@ -14,15 +14,22 @@ public class Application {
     public int age;
     public double gpa;
     public double gpaScale;
-    public int satScore;
-    public int actScore;
+    public Integer satScore;
+    public Integer actScore;
     public int felonies;
     public ApplicationStatus applicationStatus;
 
     @Builder
     @SuppressWarnings("unused")
     private static Application create(String firstName, String lastName, State state, int age, double gpa,
-                                      double gpaScale, int satScore, int actScore, int felonies) {
+                                      double gpaScale, Integer satScore, Integer actScore, int felonies) {
+
+        if (gpaScale < gpa) {
+            throw new IllegalArgumentException("GPA cannot be greater than GPA Scale");
+        } else if (satScore == null && actScore == null) {
+            throw new IllegalArgumentException("Must contain SAT Score, ACT Score, or both");
+        }
+
         Application application = new Application();
         application.setFirstName(firstName);
         application.setLastName(lastName);
@@ -31,10 +38,6 @@ public class Application {
         application.setSatScore(satScore);
         application.setActScore(actScore);
         application.setFelonies(felonies);
-
-        if (gpaScale < gpa) {
-            throw new IllegalArgumentException("GPA cannot be greater than GPA Scale");
-        }
 
         return application;
     }
